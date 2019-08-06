79f8516f84f96a3e8957d4212400fd78140e6938
Da99er <Da99er87@ya.ru>
2019-08-06 15:06:26 +0300

    meta initial scale

--- src/client/shared/templates/rootIndex.js
+++ src/client/shared/templates/rootIndex.js
@@ -6,6 +6,7 @@ exports.START = () => `<!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
 <link rel="shortcut icon" type="image/gif" src="data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=" >
 <link rel="preload" href="${RELOAD_FILES_STORAGE['index.js']}" as="script">
 <link rel="preload" href="${RELOAD_FILES_STORAGE['index.css']}" as="style">
