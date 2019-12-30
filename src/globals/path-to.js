0424528b8ef38a8283657f15038f7546360ee56b
Da99er <Da99er87@ya.ru>
2019-12-30 12:16:18 +0300

    rename helpers

--- src/globals/path-to.js
+++ src/globals/path-to.js
@@ -4,7 +4,7 @@ const DOMAIN_NAME = process.env.DOMAIN_NAME;
 
 module.exports = {
     PATH_TO_MIDDLEWARES: `${ROOTDIR}/middlewares`,
-    PATH_TO_SERVER_HELPERS: `${ROOTDIR}/helpers`,
+    PATH_TO_SERVER_HELPERS: `${ROOTDIR}/utils`,
     PATH_TO_WORKS: `${ROOTDIR}/repeatWorks`,
     PATH_TO_APIV1: `${ROOTDIR}/apiv1`,
     PATH_TO_CLIENT: '/home/vadim/Documents/react-client/src',
