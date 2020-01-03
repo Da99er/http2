a963808178b3779ff0ee92a8e835ba1d1488bf55
Da99er <Da99er87@ya.ru>
2020-01-03 14:37:42 +0300

    rm variable in repeat works

--- src/repeatWorks/index.js
+++ src/repeatWorks/index.js
@@ -5,10 +5,6 @@ const { PATH_TO_WORKS } = global.MY1_GLOBAL;
 
 const files = fs.readdirSync(PATH_TO_WORKS);
 
-const utils = {};
-
 files
     .filter((e) => e !== 'index.js')
     .map((e) => require(path.join(PATH_TO_WORKS, e)));
-
-module.exports = utils;
